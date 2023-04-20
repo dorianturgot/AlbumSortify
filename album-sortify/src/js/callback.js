e9f9775a14f50cb1ed40cd0aa98e57d84dc84cda
Dorian <dorian.turgot@gmail.com>
2023-04-20 16:25:27 -0400

    fixed background gradient banding + fixed creating list sql issue

--- album-sortify/src/js/callback.js
+++
@@ -1,9 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const code = params.get("code");
-const state = params.get("state");
-
-if (state === "authenticated" && code) {
-  const accessToken = await getAccessToken(clientId, code);
-  localStorage.setItem('accessToken', accessToken);
-  redirectToHome();
-}
